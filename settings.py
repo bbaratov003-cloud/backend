d95ed7105685675ccc5f7d857be781c58879ea8a
bbaratov003-cloud <bbaratov003@gmail.com>
2026-06-04 15:30:39 +0500

    Refactor settings.py for database and static files
    Updated settings.py to include database configuration and static files handling.

--- settings.py
+++ settings.py
@@ -1,13 +1,14 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url  # Neon bazani ulatish uchun kutubxona
 
-# .env faylini yuklash (manage.py bilan bir joyda)
+# .env faylini yuklash
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-your-secret-key'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-your-secret-key')
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 
@@ -26,6 +27,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # Sayt dizaynlari (CSS) Renderda yo'qolib qolmasligi uchun
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -57,11 +59,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+# 🗄️ TO'G'RILANGAN BAZA QISMI:
+# Agarda Renderda DATABASE_URL bo'lsa Neon'ga ulanadi, yo'q bo'lsa kompyuterda sqlite3 bo'lib ishlayveradi
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = []
@@ -72,13 +76,13 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Renderda statik fayllar uchun
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Telegram sozlamalari ( .env dan o‘qiladi)
-TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-print("TOKEN:", TELEGRAM_BOT_TOKEN)
-print("CHAT_ID:", TELEGRAM_CHAT_ID)
+# Telegram sozlamalari
+TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
+TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID')
